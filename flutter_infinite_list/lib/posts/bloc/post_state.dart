c18b609088871940c0cb6a33479d52966f4890e9
jananga99 <jananga1999@gmail.com>
2023-04-26 14:10:25 +0530

    Infinite list files added

---
+++ flutter_infinite_list/lib/posts/bloc/post_state.dart
@@ -0,0 +1,36 @@
+part of 'post_bloc.dart';
+
+enum PostStatus { initial, success, failure }
+
+// We implemented copyWith so that we can copy an instance of PostSuccess and update zero or more properties conveniently
+class PostState extends Equatable {
+  const PostState({
+    this.status = PostStatus.initial,
+    this.posts = const <Post>[],
+    this.hasReachedMax = false,
+  });
+
+  final PostStatus status;
+  final List<Post> posts;
+  final bool hasReachedMax;
+
+  PostState copyWith({
+    PostStatus? status,
+    List<Post>? posts,
+    bool? hasReachedMax,
+  }) {
+    return PostState(
+      status: status ?? this.status,
+      posts: posts ?? this.posts,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''PostState { status: $status, hasReachedMax: $hasReachedMax, posts: ${posts.length} }''';
+  }
+
+  @override
+  List<Object> get props => [status, posts, hasReachedMax];
+}
